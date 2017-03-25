a7aa404b52f57ee148106d681267376f5826eb67
Vallabh Kansagara <vrkansagara@gmail.com>
2017-03-25 13:37:50 +0530

    adding google analitics code.

--- functions.php
+++ functions.php
@@ -76,3 +76,24 @@ function disable_emojicons_tinymce( $plugins ) {
     return array();
   }
 }
+
+
+
+// function for inserting Google Analytics into the wp_head
+add_action('wp_footer', 'ga');
+function ga() {
+   if ( !is_user_logged_in() ) { // not for logged in users
+?>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62451902-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<?php
+   }
+}
